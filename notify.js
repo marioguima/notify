b50d229aa78a6d09d8011757ed5a5cf9ee242565
Mário Guimarães <marioguima@hotmail.com>
2020-12-03 02:07:56 -0300

    Removido os comentários

--- notify.js
+++ notify.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function() {
 		max = Math.floor(max);
 		return Math.floor(Math.random() * (max - min + 1)) + min;
 	}
-	
+
 	var names = ['Ana Julia','Sophia Maya','Julia Maria','Alice Dias','Manuela Farias','Isabella Santos','Laura Martins','Maria Eduarda','Giovanna Gold','Valentina Pontes','Beatriz Santos','Luiza Manuela','Helena Muniz','Maria Luiza','Isadora Ribeiro','Mariana Rios','Gabriela Dantas','Ana Clara','Ana Carla','Ana Flávia','Rafaela Duarte','Maria Clara','Isabelly Souza','Yasmin Costa','Lívia Dias','Lara Beth','Lorena Moreira','Heloísa Rosa','Melissa Maria','Sarah Souza','Ana Luiza','Letícia Brito','Nicole Santos','Ana Beatriz','Emanuely Silva','Esther Korny','Lavínia Bety','Marina Grabiela','Cecília Gomes','Rebeca Fausto','Vitória Dias','Maria Fernanda','Larissa Souza','Clara Dias','Carolina Souza','Bianca Coelho','Alícia Moreira','Fernanda Clock','Gabrielly Kinsk','Catarina Borges','Ana Laura','Emilly Lisa','Eduarda Bruna','Amanda Maria','Pietra Glosk','Agatha Cris','Milena Souza','Maria Alice'];
 
 	toastr.options = {
@@ -29,17 +29,13 @@ jQuery(document).ready(function() {
     
     function showName()
     {
-        //   document.getElementById("qtd_nomes").innerHTML = names.length;
         var nName = Math.floor(Math.random() * names.length);
         var name = names[nName];
         names.splice(nName, 1);
-        //   document.getElementById("nome").innerHTML = name;
         toastr["success"]("Garantiu sua vaga agora!", name);
         clearInterval(handleShowName);
-        //   document.getElementById("intervalo").innerHTML = '';
         if (names.length > 0) {
             var interval = getRandomInt(5,10) * 1000;
-            // document.getElementById("intervalo").innerHTML = interval;
             handleShowName = setInterval(showName, interval);
         }
     }
